2c1a518e59362491754cb3a786d61560b807df44
NexusJungler <olvmarchand@gmail.com>
2016-04-08 14:39:23 +0200

    Added files via upload

--- Education/classes/Entity/algo.php
+++ Education/classes/Entity/algo.php
@@ -4,33 +4,30 @@ namespace Educ\Entity;
 
 class algo {
     private  $dataImg;
-    private $offset;                 // deprecated!
+    private $offset;
     private $lengthline;
     private $head;
     private $width;
-    private $count = 0;
-    public $coord = ['debut'=>0, 'fin'=>0];
-    public $cells = [];
-    private $line = 520;
+    private $color;
     private $find = false;
-    private  $srcfile = null;
+    private $srcfile = null;
 
     public function  construct__()
     {
     }
-    public function  readCell($decalX, $decalY) {
+    public function  readCell($item) {
         $file = 'temp/CellMn.bmp';
         $this->head = file_get_contents($file, NULL, NULL, 0, 62);
         $this->dataImg = file_get_contents($file, NULL, NULL, 62);
         $this->width = $this->getInfo('largeur');
+        $this->color = $this->getInfo('profondeur')/8;
         $this->lengthline = $this->getFormat($this->width);
+
         $wordCell = [];
-        for($y = $decalY, $Comp = 0; $Comp < 6; $y += 106, $Comp++) {
-            //$wordCell[$Comp] ='';
-            for ($x = 1; $x < 6; $x++) {
-                $fill = $this->checkcontent($decalX+40 * ($x-1), $y);
-                $wordCell[$Comp*5+$x]=$fill;
-            }
+        $pos = $this->getposition($item*100);
+        for ($x = 1; $x < 6; $x++) {
+                $fill = $this->checkcontent($pos[0] + 40 * ($x-1), $pos[1]);
+                $wordCell[$x]=$fill;
         }
         return $wordCell;
     }
@@ -47,9 +44,10 @@ class algo {
         $offsImg = $this->getInfo('image_position');
         fseek($this->srcfile, $offsImg);
         $this->width = $this->getInfo('largeur');
-        $this->lengthline = $this->getFormat($this->width);
-        $jump = $this->getFormat($this->width, true);
+        $this->lengthline = $this->getFormat();
+        $jump = $this->getFormat(true);
         $newImgSize = $this->lengthline * $this->getInfo('hauteur');
+        $this->color = $this->getInfo('profondeur')/8;
 
         // modification du header
         $head = $this->head;
@@ -59,28 +57,17 @@ class algo {
             $head = substr_replace($head, pack($format, $value), $posInfo[$key], $long);
         }
         $head .= $endhead;
-
- /*
-        echo '<br>' . bin2hex($this->head) . '<br>';
-        echo bin2hex($head) . '<br>';
-        echo '<br>ancienne taille fichier: '. $this->getInfo('fichier_taille');
-        echo '<br>nouvelle taille image: '. $newImgSize;
-        echo '<br>resolution H: '. $this->width;
-        echo '<br>octets par ligne: '. $this->lengthline;
-        echo '<br>saut: ' . $jump . '<br><br>';                 */
-
-          fwrite($newfile, $head);
+        fwrite($newfile, $head);
 
         // traitement de chaque ligne de l'image, repositionnement du pointeur de fichier en fonction du codage d'origine des lignes (octets vides à ignorer)
-        $total='';
         for($z=0; $z<$this->getInfo('hauteur'); $z++) {
            if($z>0 && $jump>0) {fseek($this->srcfile, ftell($this->srcfile)+$jump);}
 
            // extraction d'une seule ligne
-            $imageline = fread($this->srcfile, $this->width*3);
+            $imageline = fread($this->srcfile, $this->width*$this->color);
             for ($i = 0; $i < $this->width; $i ++) {
-                $pix = bin2hex(substr($imageline, $i*3, 3));
-                if (hexdec($pix)<10000000) {                         // 1:2 = 8 388 608
+                $pix = bin2hex(substr($imageline, $i*$this->color, $this->color));
+                if (hexdec($pix) < 0xee6b2800) {    // 4000 000 000
                     $byteExtract[$i]= 0;
                 }else {
                     $byteExtract[$i]=1;
@@ -112,62 +99,55 @@ class algo {
                 $data = $this->getIntValue($value);
                 $store .= pack("c", $data);
             }
-           $add = strlen($store);
-           $total += $add;
            fwrite($newfile, $store);
        }
-      //  echo 'Fichier converti, '. $total .' octets ont ete enregistres!<br>';
         fclose($this->srcfile);
         fclose($newfile);
-       unlink($file);
+        unlink($file);
     }
-    public  function  getdecalage() {
-        $map = [0,0,1,0,1,1,0,1,2,0,2,1,1,2,0,2,2,2] ;
-        $goodpos=0; $level=1; $excellent=0;  $i=0;
-        $spare = $strike = 0;   $decal = [];  $bloc=0;
-        while($i<count($map) && $goodpos<$level || $excellent<4) {
-             var_dump($i);
-            $analyse = $this->readCell($map[$i],$map[$i+1]);
-            foreach($analyse as $key => $value) {
-                if($value >60) {unset($analyse[$key]);}
-            }
-            $goodpos = count($analyse);
-            $excellent = 0;
-            $level = $goodpos;      //*2/3?
-            foreach($analyse as $value) {
-                if($value > 5 && $value <20) {$goodpos--;}
-                if($value==0) {$excellent++;}
-            }
-            if($goodpos==$level && $excellent>2 && $strike==0) {$strike = (($i/2)+1 ); echo 'STRIKE!';}
-            if($goodpos==$level && $strike==0 && $spare==0) {$spare = (($i/2)+1 ); echo 'SPARE!';}
-            /*    echo '<br>test= ' . (($i/2)+1);
-                echo '<br>excellent= ' . $excellent;
-                echo '<br>level= ' . $level;
-                echo '<br>goodpos= ' . $goodpos. '<br>';
-                var_dump($analyse);        */
-            $i+=2;
+    public  function pass_to_black() {
+        $file = 'temp/Qrcod.bmp';
+        $this->srcfile = fopen($file, "r+");
+        $this->head =  fread($this->srcfile, 54);
+        $offsImg = $this->getInfo('image_position');
+        fseek($this->srcfile, $offsImg);
+        $this->width = $this->getInfo('largeur');
+        $this->color = $this->getInfo('profondeur')/8;
+        $this->lengthline = $this->width*$this->color;
+
+        $white_pix = ''; $black_pix = '';
+        for($i=0; $i<$this->color; $i++) {
+            $white_pix .= 'ff';
+            $black_pix .= '00';
         }
-        if($i==count($map)) {
-            if($strike!=0) {
-                $decal[0] = $map[($strike-1)*2];
-                $decal[1] = $map[($strike-1)*2+1];
-                $bloc = $strike;
+        for($z=0; $z<$this->getInfo('hauteur'); $z++) {
+            // extraction d'une seule ligne
+
+            $store='' ;
+            $imageline = fread($this->srcfile, $this->lengthline);
+            for ($i = 0; $i < $this->width; $i ++) {
+                $pix = bin2hex(substr($imageline, $i*$this->color, $this->color));
+
+                if (hexdec($pix) > 0xd09dc300) {       // 3500000000
+                    $store .= pack('h*', $white_pix);
+                } else {
+                    $store .= pack('h*', $black_pix);
+                }
             }
-            if($spare!=0 && $strike==0) {
-                $decal[0] = $map[($spare-1)*2];
-                $decal[1] = $map[($spare-1)*2+1];
-                $bloc=$spare;
-            }
-            if($strike==0 && $spare==0) {echo 'What\'s the loadMeans!';$bloc='not found!';}
-        } else {
-            echo'SUPERBALL!';
-            $decal[0] = $map[$i-2];
-            $decal[1] = $map[($i-1)];
-            $bloc=$i/2;
+            fseek($this->srcfile, ftell($this->srcfile) - $this->lengthline);
+            fwrite($this->srcfile, $store);
         }
-        echo '<br>bloc ideal: ' . $bloc;
-        echo '<br>le decalage est de: '  . $decal[0] .' et '.  $decal[1];
-        echo '<br>le placement a ete optimise! Let\'s continue!' ;
+        fclose($this->srcfile);
+        exec("convert " . $file . " temp/Qrcod.png");
+        unlink($file);
+    }
+    private  function  getposition($line) {
+        $decal = [];
+        while ($decal == null) {
+            $line++;
+            $decal = $this->checkline($line);
+        }
+        if($this->getvertical($decal[0]-5, $decal[1]-5) < 75) {$decal[0]++;}
         return $decal;
     }
     private function  getpix($x, $y) {
@@ -183,24 +163,17 @@ class algo {
         for ($i = 0; $i < 33; $i++) {
             $vertic .= $this->getpix($x, $y + $i);
         }
-        var_dump($vertic);
         for ($i = 0; $i < strlen($vertic); $i++) {
             $pix = substr($vertic, $i, 1);
-            if ($pix == 0) {
+            if ($pix == '0') {
                 $result++;
             }
         }
-        $result = ($result / 33) * 100 .'%';
-        return $result;
+        $result = (int)($result/33*100);
+         return $result;
     }
-    public function getline($numline)         // toujours ajouter 1 par rapport à paint pour ajuster l'axe vertical    (ligne 0 n'existe pas pour Algo)
+    private function getline($numline)
     {
-        $file = 'temp/CellMn.bmp';
-        $this->head = file_get_contents($file, NULL, NULL, 0, 62);
-        $this->dataImg = file_get_contents($file, NULL, NULL, 62);
-        $this->width = $this->getInfo('largeur');
-        $this->lengthline = $this->getFormat($this->width);
-        /********************************************/
         $result = '';
         $start = $this->lengthline * $numline;
         $this->offset = $this->getInfo('image_taille') - $start;
@@ -210,18 +183,15 @@ class algo {
             $binary = $this->binExtract($offset);
             $result .= $binary;
         }
-
-       // var_dump($result);
         return $result;
     }
-    public function checkline($line, $pos=0)
+    private function checkline($line, $pos=0)
     {
-
+        $this->find = false;
         $content = substr($this->getline($line), $pos);
         $pix = [];   $result=[];
         $last = 2;
         $tampon = null;
-
         for ($i = 0; $i < strlen($content); $i++) {
             $pix[$i] = substr($content, $i, 1);
         }
@@ -229,10 +199,8 @@ class algo {
                 if ($last != 2 && $last != $value && !$this->find) {
                     if ($last == 0 && $value == 1) {
                         if ($offset - $tampon >= 30) {
-                            $this->coord['debut'] = $tampon + $pos;
-                            $this->coord['fin'] = $offset + $pos - 1;
                             $this->find = true;
-                            $result[0] = $this->coord['debut'] + 5;
+                            $result[0] = $tampon + $pos + 5;
                             $result[1] = $line + 5;
                         }
                     }
@@ -279,6 +247,7 @@ class algo {
     private function  getInfo($data)
     {
         $offset = null;
+        $capacity = true;
         switch ($data) {
             case 'largeur':
                 $offset = 18;
@@ -286,8 +255,9 @@ class algo {
             case 'hauteur':
                 $offset = 22;
                 break;
-            case 'profondeur':          // attention ->word et non qword
+            case 'profondeur':
                 $offset = 28;
+                $capacity = false;
                 break;
             case 'image_position':
                 $offset = 10;
@@ -299,17 +269,23 @@ class algo {
                 $offset = 2;
                 break;
         }
-        $qword = substr($this->head, $offset, 4);
-        $result = unpack('V', $qword);
-        return $result[1];
+        if ($capacity) {
+            $qword = substr($this->head, $offset, 4);
+            $result = unpack('V', $qword);
+            return $result[1];
+        } else {
+            $word = substr($this->head, $offset, 2);
+            $result = unpack('v', $word);
+            return $result[1];
+        }
     }
     function isMultiple($nombre, $multiple){
         if(($nombre % $multiple) == 0)  {return true;}
         else {return false;}
     }
-    private function getFormat($largeur, $mode=false) {
-        $i =$result=$base=0;
-        if($mode) {$base = $largeur *3;} else {$base = (int)($largeur/8);}
+    private function getFormat($mode=false) {
+        $i = $result = $base = 0;
+        if($mode) {$base = $this->width * $this->color;} else {$base = (int)($this->width/8);}
         while(!$this->isMultiple($base+$i, 4))  {
             $i++;
         }
@@ -340,44 +316,8 @@ class algo {
             $byte = $byte % (256 / $i);
             $binstring .= $result;
         }
-        // echo base_convert('eb7bdef7bac8',16,2) . '</br>';        // limité à 13 caractères!
         return $binstring;
-
     }
-    private function findcell() {
-        while(!$this->find) {
-            $this->line++;
-            $this->checkline($this->line);
-        }
-        echo $this->line;
-        if($this->count>0) {$this->checkline($this->line, $this->coord['fin']);}
-        var_dump($this->coord);
-        $caf = $this->coord['debut'];
-        $toto = $this->getvertical($caf, $this->line);
-        $titi =  $this->getvertical($caf-1, $this->line);
-        echo $toto; echo ' ' . $titi;
-        if($toto-$titi>=75) {$this->cells[$this->count]['abs'] = $caf ;}
-        if($titi-$toto>=75)  {$this->cells[$this->count]['abs'] = $caf-1 ;}
-        $this->cells[$this->count]['ord'] = $this->line;
-        $this->count++;
-       // if($this->coord['fin']>220) {$this->line++;}
-        var_dump($this->cells);
-     }
-         /* function decompose($word) {
-          $byte = [];
-          $bit4 = [];
-          $i=0;   $j=0;
-           foreach ($word as $value)    {
-              $byte[$j] = (int)($value/256);
-              $byte[$j+1] = $value%256;
-              $j+=2;
-          }
-          foreach ($byte as $value) {
-              $bit4[$i] = (int)($value/16);
-              $bit4[$i+1] =  $value%16;
-              $i+=2;
-          }
-          return $bit4;
-      }     */
+
 }
 
